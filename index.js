adb06744047810680d13b7cd837f0afabade6507
Maksim <bankov2016@gmail.com>
2023-06-20 03:58:19 +0300

    init: lottie task

---
+++ index.js
@@ -0,0 +1,23 @@
+let sticer1_animation = bodymovin.loadAnimation({
+    container: document.getElementById("container-sticers"),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data_coin1.json',
+});
+
+let sticer2_animation = bodymovin.loadAnimation({
+    container: document.getElementById("container-sticers"),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data_coin2.json',
+});
+
+let sticer3_animation = bodymovin.loadAnimation({
+    container: document.getElementById("container-sticers"),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data_coin3.json',
+});
