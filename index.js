cabbf200eb0cc83712b1c0609a219294f12683b4
Maksim <bankov2016@gmail.com>
2023-07-03 02:10:35 +0300

    init: cup sticker

--- index.js
+++ index.js
@@ -27,5 +27,37 @@ let sticerCup_1_animation = bodymovin.loadAnimation({
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: '.json',
+    path: 'Cup_1.json',
+});
+
+let sticerCup_2_animation = bodymovin.loadAnimation({
+    container: document.getElementById("container-sticers"),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'Cup_2.json',
+});
+
+let sticerCup_3_animation = bodymovin.loadAnimation({
+    container: document.getElementById("container-sticers"),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'Cup_3.json',
+});
+
+let sticerCup_4_animation = bodymovin.loadAnimation({
+    container: document.getElementById("container-sticers"),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'Cup_4.json',
+});
+
+let sticerCup_5_animation = bodymovin.loadAnimation({
+    container: document.getElementById("container-sticers"),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'Cup_5.json',
 });
